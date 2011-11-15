a35fcc125c33e2642a7fca7f7bbac65ec24e3ff5
kmm <kmm@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-15 22:12:12 +0000

    dir struct, NEWS
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/deoptim@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- deoptim/pkg/DEoptim/R/zzz.R
+++
@@ -1,7 +0,0 @@
-".onLoad" <- function (lib, pack)
-{
-  library.dynam(pack, pack, lib)
-  packageStartupMessage("\nDEoptim package",
-   "\nDifferential Evolution algorithm in R",
-   "\nAuthors: D. Ardia, K. Mullen, B. Peterson and J. Ulrich\n")
-}
